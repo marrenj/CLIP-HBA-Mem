d9b80ed3e9e2ab6f9e304b3e3205b81a5fedf9a7
stephenczhao <stephenzhao9519@gmail.com>
2025-02-17 16:50:31 -0600

    individual training restructure

--- CLIP-HBA/inference_meg_group.py
+++ CLIP-HBA/inference_meg_group.py
@@ -4,31 +4,31 @@ def main():
     # Define configuration
     config = {
         # Data paths
-        'img_dir': './Data/test_images/',
+        'img_dir': './Data/test_images/', # Path to the directory containing images
         'n_dim': 66,  # options: 66
         
         # Model parameters
-        'backbone': 'ViT-L/14',
-        'model_path': './models/cliphba_meg_group.pth',
-        'save_folder': '../output/cliphba_meg_things_demo/things/',
+        'backbone': 'ViT-L/14', # backbone model for CLIP. All published CLIP-HBA models use ViT-L/14
+        'model_path': './models/cliphba_meg_group.pth', # Path to the trained model
+        'save_folder': '../output/cliphba_meg_things_demo/things/', # Path to save the output
         'batch_size': 128,
-        'vision_layers': 24,
-        'transformer_layers': 1,
-        'rank': 32,
+        'vision_layers': 24, # Number of vision layers in the CLIP model that was trained, default pretrained CLIP-HBA-MEG model has 24 layers
+        'transformer_layers': 1, # Number of transformer layers in the CLIP model that was trained, default pretrained CLIP-HBA-MEG model has 1 layer
+        'rank': 32, # Rank of DoRA modules, default pretrained CLIP-HBA-MEG model has rank 32
         
         # Time parameters
-        'ms_start': -100,
-        'ms_end': 1300,
-        'ms_step': 5,
-        'inference_start': -100,
-        'inference_end': 1300,
-        'inference_step': 5,
-        'train_window_size': 15,
+        'ms_start': -100, # Start time in ms, follow the parameters used in the training. Default pretrained CLIP-HBA-MEG model uses -100 ms
+        'ms_end': 1300, # End time in ms, follow the parameters used in the training. Default pretrained CLIP-HBA-MEG model uses 1300 ms
+        'ms_step': 5, # Time step in ms, follow the parameters used in the training. Default pretrained CLIP-HBA-MEG model uses 5 ms
+        'inference_start': -100, # Start time for inference in ms, this can be changed based on the desired inference window
+        'inference_end': 1300, # End time for inference in ms, this can be changed based on the desired inference window
+        'inference_step': 5, # Time step for inference in ms, this can be changed based on the desired inference window
+        'train_window_size': 15, # Window size for training in ms, follow the parameters used in the training. Default pretrained CLIP-HBA-MEG model uses 15 ms
         
         # Device settings
-        'cuda': 'cuda:1',
-        'load_pretrained': True,
-        'random_seed': 1
+        'cuda': 'cuda:1', # Device to run the model on, 'cuda:0' for GPU 0, 'cuda:1' for GPU 1, 'cuda' for all GPUs
+        'load_pretrained': True, # Must load pretrained model
+        'random_seed': 1 # default pre-trained CLIP-HBA models all use random seed 1
     }
 
     # Run inference
---
+++ CLIP-HBA/train_meg_individual.py
@@ -0,0 +1,66 @@
+from functions.train_meg_individual_pipeline import run_individual_training
+
+def main():
+    # Define configuration
+    config = {
+        # Data paths
+        'csv_file': './Data/Cichy/cichy_100_images_list_train.csv',
+        'img_dir': './Data/Cichy/stimuli',
+        'rdm_dir': "./Data/Cichy/Cichy_MEG_RDM_rescaled.npy",
+        'model_save_folder': './models/cliphba_meg_individual_test',
+        'n_dim': 66,
+        
+        # Participant settings
+        'n_participants': 15,
+        'skip_id': [],
+        
+        # Model parameters
+        'backbone': 'ViT-L/14',
+        'epochs': 40,
+        'fw_tuning_epochs': 10,
+        'batch_size': 40,
+        'train_portion': 80/100,
+        'smoothen_window': 5,
+        
+        # Time parameters
+        'ms_start': -100,
+        'ms_end': 1000,
+        'ms_step': 5,
+        'train_start': -100,
+        'train_end': 1000,
+        'train_step': 5,
+        'train_window_size': 15,
+        
+        # Learning parameters
+        'lr_1': 0,
+        'lr_2': 3e-5,
+        'fw_lr_1': 3e-3,
+        'fw_lr_2': 3e-3,
+        'early_stopping_patience_0': 3,
+        'early_stopping_patience_1': 10,
+        
+        # Model architecture
+        'vision_layers': 24,
+        'transformer_layers': 1,
+        'rank': 6,
+        'dora_d_state': True,
+        'dora_m_state': False,
+        
+        # Loss weights
+        'p_weight': 1,
+        'm_weight': 0.15,
+        'g_weight': 0.1,
+        
+        # Other settings
+        'cuda': 1,
+        'random_seed': 1,
+        'noise_scale': 1,
+        'text_encoder_path': "./models/cliphba_behavior_text_encoder/cliphba_behavior_text_encoder.pth",
+        'freeze_text': True
+    }
+
+    # Run training
+    run_individual_training(config)
+
+if __name__ == '__main__':
+    main()
